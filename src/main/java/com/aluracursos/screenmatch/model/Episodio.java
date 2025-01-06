114e0163b351e849b601a5aed886c948254fda6b
valadezjessica <jessica.jessicavaladezv@gmail.com>
2025-01-05 22:30:19 -0600

    Se crea la clase Episodio dentro del packg model

---
+++ src/main/java/com/aluracursos/screenmatch/model/Episodio.java
@@ -0,0 +1,51 @@
+package com.aluracursos.screenmatch.model;
+
+import java.time.LocalDate;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Double calificacion;
+    private Integer numeroEpisodio;
+    private LocalDate fechaDeLanzamiento;
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public LocalDate getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public void setFechaDeLanzamiento(LocalDate fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+}
